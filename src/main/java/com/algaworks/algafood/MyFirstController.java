99a87c062cb521ca403e1c6d056d131266d590de
Wilian <wilian.hw@hotmail.com>
2023-09-23 12:24:52 -0300

    Controller of test

---
+++ src/main/java/com/algaworks/algafood/MyFirstController.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafood;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyFirstController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello!";
+    }
+}
