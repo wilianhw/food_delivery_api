171f95d6e5c9aa614d70b849b790a65d70e3987e
Wilian <wilian.hw@hotmail.com>
2023-10-09 21:57:13 -0300

    Created endpoint to create a new Restaurante.

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -4,11 +4,9 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestaurante;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,6 +35,18 @@ public class RestauranteController {
             return ResponseEntity.ok(restaurante);
         } catch (EntidadeNaoEncontradaException e) {
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> cadastrar(@RequestBody Restaurante restaurante) {
+        try {
+            restaurante = cadastroRestaurante.salvar(restaurante);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 }
--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestaurante.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestaurante.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.service;
 
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,11 @@ import java.util.Optional;
 public class CadastroRestaurante {
 
     private final RestauranteRepository restauranteRepository;
+    private final CadastroCozinha cadastroCozinha;
 
-    public CadastroRestaurante(RestauranteRepository restauranteRepository) {
+    public CadastroRestaurante(RestauranteRepository restauranteRepository, CadastroCozinha cadastroCozinha) {
         this.restauranteRepository = restauranteRepository;
+        this.cadastroCozinha = cadastroCozinha;
     }
 
     public Restaurante buscarOuFalhar(Long restauranteId) {
@@ -26,5 +29,14 @@ public class CadastroRestaurante {
         }
 
         return optionalRestaurante.get();
+    }
+
+    public Restaurante salvar(Restaurante restaurante) {
+        Long cozinhaId = restaurante.getCozinha().getId();
+        Cozinha cozinha = cadastroCozinha.buscarOuFalhar(cozinhaId);
+
+        restaurante.setCozinha(cozinha);
+
+        return restauranteRepository.save(restaurante);
     }
 }
