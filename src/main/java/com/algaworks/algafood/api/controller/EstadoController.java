b8439f5b08674736860083069af652d1b97075e7
Wilian <wilian.hw@hotmail.com>
2023-10-10 22:19:36 -0300

    Created CRU for Estado.

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -1,10 +1,14 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.EstadoRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.algaworks.algafood.domain.service.CadastroEstado;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,13 +17,52 @@ import java.util.List;
 public class EstadoController {
 
     private final EstadoRepository estadoRepository;
+    private final CadastroEstado cadastroEstado;
 
-    public EstadoController(EstadoRepository estadoRepository) {
+    public EstadoController(EstadoRepository estadoRepository, CadastroEstado cadastroEstado) {
         this.estadoRepository = estadoRepository;
+        this.cadastroEstado = cadastroEstado;
     }
 
     @GetMapping
     public List<Estado> listar() {
         return estadoRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity cadastrar(@RequestBody Estado estado) {
+        estado = cadastroEstado.salvar(estado);
+
+        return ResponseEntity.ok(estado);
+    }
+
+    @PutMapping("/{estadoId}")
+    public ResponseEntity atualizar(
+            @PathVariable Long estadoId,
+            @RequestBody Estado estado
+    ) {
+        try {
+            Estado estadoAtual = cadastroEstado.buscarOuFalhar(estadoId);
+
+            BeanUtils.copyProperties(estado, estadoAtual, "id");
+
+            return ResponseEntity.ok(estadoAtual);
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{restauranteId}")
+    public ResponseEntity<?> deletar(@PathVariable Long restauranteId) {
+        try {
+            Estado estado = cadastroEstado.buscarOuFalhar(restauranteId);
+
+            cadastroEstado.apagar(estado);
+            return ResponseEntity.noContent().build();
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.notFound().build();
+        } catch (EntidadeEmUsoException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroEstado.java
@@ -0,0 +1,46 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CadastroEstado {
+
+    private final EstadoRepository estadoRepository;
+
+    public CadastroEstado(EstadoRepository estadoRepository) {
+        this.estadoRepository = estadoRepository;
+    }
+
+    public Estado buscarOuFalhar(Long restauranteId) {
+        Optional<Estado> optionalEstado = estadoRepository.findById(restauranteId);
+
+        if (optionalEstado.isEmpty()) {
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Estado com código %d não encontrado", restauranteId)
+            );
+        }
+
+        return optionalEstado.get();
+    }
+
+    public Estado salvar(Estado estado) {
+        return estadoRepository.save(estado);
+    }
+
+    public void apagar(Estado estado) {
+        try {
+            estadoRepository.delete(estado);
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(
+                    String.format("Estado de código %d não pode ser removido pois está em uso", estado.getId())
+            );
+        }
+    }
+}
