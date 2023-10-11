bd29a83e2c4811c8679fcf024c1329415639cec4
Wilian <wilian.hw@hotmail.com>
2023-10-10 21:49:36 -0300

    Created endpoint to update a new Restaurante.

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -4,6 +4,7 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestaurante;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,7 +29,7 @@ public class RestauranteController {
     }
 
     @GetMapping("/{restauranteId}")
-    public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId) {
+    public ResponseEntity<Restaurante> buscarOuFalhar(@PathVariable Long restauranteId) {
         try {
             Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
 
@@ -39,12 +40,29 @@ public class RestauranteController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<?> cadastrar(@RequestBody Restaurante restaurante) {
         try {
             restaurante = cadastroRestaurante.salvar(restaurante);
 
             return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{restauranteId}")
+    public ResponseEntity<?> atualizar(
+            @PathVariable Long restauranteId,
+            @RequestBody Restaurante restaurante
+    ) {
+        try {
+            Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
+
+            BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+
+            cadastroRestaurante.salvar(restauranteAtual);
+
+            return ResponseEntity.ok(restauranteAtual);
         } catch (EntidadeNaoEncontradaException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
