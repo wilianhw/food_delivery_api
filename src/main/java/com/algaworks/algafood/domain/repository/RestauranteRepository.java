fa648a70057bf70e7f623469de4f872f757cb167
Wilian <wilian.hw@hotmail.com>
2023-10-18 22:42:05 -0300

    Implemented custom jpa repository.

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -1,8 +1,6 @@
 package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestauranteRepository extends
-        JpaRepository<Restaurante, Long>,
+        CustomJpaRepository<Restaurante, Long>,
         CustomizedRestauranteRepository,
         JpaSpecificationExecutor<Restaurante> {
     @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CustomJpaRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import com.algaworks.algafood.domain.repository.CustomJpaRepository;
+import jakarta.persistence.EntityManager;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import java.util.Optional;
+
+public class CustomJpaRepositoryImpl<T, ID> extends SimpleJpaRepository<T, ID> implements CustomJpaRepository<T, ID> {
+
+    private final EntityManager manager;
+    public CustomJpaRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
+        super(entityInformation, entityManager);
+
+        this.manager = entityManager;
+    }
+
+    @Override
+    public Optional<T> buscarPrimeiro() {
+        var jpql = "from " + getDomainClass().getName();
+
+        T entity = manager.createQuery(jpql, getDomainClass())
+                .setMaxResults(1)
+                .getSingleResult();
+
+        return Optional.ofNullable(entity);
+    }
+}
