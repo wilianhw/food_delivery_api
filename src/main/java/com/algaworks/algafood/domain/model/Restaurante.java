b714c8632013432e84196178bdfb332de7983d82
Wilian <wilian.hw@hotmail.com>
2023-10-19 21:47:32 -0300

    Implemented many to many mapping.

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -26,4 +28,10 @@ public class Restaurante {
     @ManyToOne
     @JoinColumn(name = "cozinha_id")
     private Cozinha cozinha;
+
+    @ManyToMany
+    @JoinTable(name = "restaurante_forma_pagamento",
+            joinColumns = @JoinColumn(name = "restaurante_id"),
+            inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+    public List<FormaPagamento> formasPamento = new ArrayList<>();
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -18,10 +18,16 @@ INSERT INTO algafood.cidade (nome, estado_id) VALUES('Lavras', 1);
 INSERT INTO algafood.cidade (nome, estado_id) VALUES('São Paulo', 1);
 
 -- Forma de pagamento
-INSERT INTO algafood.forma_pagamento (descricao) VALUES ('Crédito');
-INSERT INTO algafood.forma_pagamento (descricao) VALUES ('Debito');
+INSERT INTO algafood.forma_pagamento (id, descricao) VALUES (1, 'Crédito');
+INSERT INTO algafood.forma_pagamento (id, descricao) VALUES (2, 'Debito');
 
 -- Permissão
 INSERT INTO algafood.permissao (nome, descricao) VALUES ('CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
 INSERT INTO algafood.permissao (nome, descricao) VALUES ('EDITAR_COZINHAS', 'Permite editar cozinhas')
 
+-- Restaurante forma pagamento
+INSERT INTO algafood.restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) VALUES(1, 1);
+INSERT INTO algafood.restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) VALUES(2, 1);
+INSERT INTO algafood.restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) VALUES(3, 1);
+INSERT INTO algafood.restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) VALUES(1, 2);
+INSERT INTO algafood.restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) VALUES(3, 2);
