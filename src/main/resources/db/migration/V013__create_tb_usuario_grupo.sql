42b446580e6fc2c9aa4e3771653783e55448d96f
Wilian <wilian.hw@hotmail.com>
2023-10-22 18:51:06 -0300

    Script to create tb usuario_grupo.

---
+++ src/main/resources/db/migration/V013__create_tb_usuario_grupo.sql
@@ -0,0 +1,12 @@
+CREATE TABLE usuario_grupo (
+	usuario_id bigserial NOT NULL,
+	grupo_id bigserial NOT NULL,
+
+	PRIMARY KEY (usuario_id, grupo_id)
+);
+
+ALTER TABLE usuario_grupo ADD CONSTRAINT fk_usuario_grupo_usuario
+FOREIGN KEY (usuario_id) REFERENCES usuario (id);
+
+ALTER TABLE usuario_grupo ADD CONSTRAINT fk_usuario_grupo_grupo
+FOREIGN KEY (grupo_id) REFERENCES grupo (id);
