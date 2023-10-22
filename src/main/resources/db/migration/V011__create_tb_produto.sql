c171a2fa761e2ee3fba0d338f9929e3c57330796
Wilian <wilian.hw@hotmail.com>
2023-10-22 18:50:50 -0300

    Script to create tb produto.

---
+++ src/main/resources/db/migration/V011__create_tb_produto.sql
@@ -0,0 +1,13 @@
+CREATE TABLE produto (
+	id bigserial NOT NULL,
+	restaurante_id bigserial NOT NULL,
+	nome varchar(80) NOT NULL,
+	descricao varchar(80) NOT NULL,
+	preco decimal(10, 2),
+	ativo boolean NOT NULL,
+
+	PRIMARY KEY (id)
+);
+
+ALTER TABLE produto ADD CONSTRAINT fk_produto_restaurante
+FOREIGN KEY (restaurante_id) REFERENCES restaurante (id);
