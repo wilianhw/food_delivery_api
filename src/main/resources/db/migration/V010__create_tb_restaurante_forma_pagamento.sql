6aba6db78bab461f0f840731a70ad4db9d6ce62c
Wilian <wilian.hw@hotmail.com>
2023-10-22 18:50:43 -0300

    Script to create tb forma_pagamento.

---
+++ src/main/resources/db/migration/V010__create_tb_restaurante_forma_pagamento.sql
@@ -0,0 +1,12 @@
+CREATE TABLE restaurante_forma_pagamento (
+	restaurante_id bigserial NOT NULL,
+	forma_pagamento_id bigserial NOT NULL,
+
+	PRIMARY KEY (restaurante_id, forma_pagamento_id)
+);
+
+ALTER TABLE restaurante_forma_pagamento ADD CONSTRAINT fk_restaurante_forma_pagamento_restaurante
+FOREIGN KEY (restaurante_id) REFERENCES restaurante (id);
+
+ALTER TABLE restaurante_forma_pagamento ADD CONSTRAINT fk_restaurante_forma_pagamento_forma_pagamento
+FOREIGN KEY (forma_pagamento_id) REFERENCES forma_pagamento (id);
