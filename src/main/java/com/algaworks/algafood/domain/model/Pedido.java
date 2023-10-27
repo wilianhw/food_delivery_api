6e39b41b979ad829a0bf0c3d83ee8849f4bebee1
Wilian <wilian.hw@hotmail.com>
2023-10-26 22:05:02 -0300

    Created entity Pedido.

---
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -0,0 +1,60 @@
+package com.algaworks.algafood.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Pedido {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal subtotal;
+
+    @Column(nullable = false)
+    private BigDecimal taxaFrete;
+
+    @Column(nullable = false)
+    private BigDecimal valorTotal;
+
+    @Embedded
+    private Endereco endereco;
+
+    private StatusPedido status;
+
+    @CreationTimestamp
+    private LocalDateTime dataCriacao;
+
+    private LocalDateTime dataConfirmacao;
+
+    private LocalDateTime dataCancelamento;
+
+    private LocalDateTime dataEntrega;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_cliente_id", nullable = false)
+    private Usuario cliente;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurante restaurante;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private FormaPagamento formaPagamento;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens = new ArrayList<>();
+}
