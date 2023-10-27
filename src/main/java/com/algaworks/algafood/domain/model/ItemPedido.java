0477d45b4af4a82b37f21536ebf9eac236c8eb78
Wilian <wilian.hw@hotmail.com>
2023-10-26 22:05:12 -0300

    Created entity ItemPedido.

---
+++ src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class ItemPedido {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
+    @Column(nullable = false)
+    private BigDecimal precoUnitario;
+
+    @Column(nullable = false)
+    private BigDecimal precoTotal;
+
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Produto produto;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Pedido pedido;
+}
