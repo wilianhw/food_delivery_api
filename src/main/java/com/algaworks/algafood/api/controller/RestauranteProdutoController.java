8983d1a1404c2028f430960d4d1ebc3084c0d594
Wilian <wilian.hw@hotmail.com>
2023-12-16 08:47:44 -0300

    Fixed uri to RestauranteProdutoController.

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/restaurante/{restauranteId}/produtos")
+@RequestMapping("/restaurantes/{restauranteId}/produtos")
 public class RestauranteProdutoController {
 
     private final ProdutoModelAssembler produtoModelAssembler;
