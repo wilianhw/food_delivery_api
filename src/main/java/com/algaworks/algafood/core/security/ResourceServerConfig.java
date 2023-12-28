1b185654604a829c81a7973988a8253f88067c52
wilian.souza <wilian.souza@dtidigital.com.br>
2023-12-28 13:49:37 -0300

    Implemented resource server config with client credentials and opaque token.

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -3,35 +3,25 @@ package com.algaworks.algafood.core.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity
-@EnableWebSecurity
 public class ResourceServerConfig {
 
-    private final ResourceServerProperties resourceServerProperties;
-
-    public ResourceServerConfig(ResourceServerProperties resourceServerProperties) {
-        this.resourceServerProperties = resourceServerProperties;
-    }
-
     @Bean
-    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .formLogin(Customizer.withDefaults())
-                .csrf(CsrfConfigurer::disable)
                 .cors(Customizer.withDefaults())
-                .oauth2ResourceServer(oauth2 -> oauth2.opaqueToken(token -> {
-                    token.introspectionUri(resourceServerProperties.getIntrospectionUri());
-                    token.introspectionClientCredentials(
-                            resourceServerProperties.getClientId(),
-                            resourceServerProperties.getClientSecret());
-                }))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeHttp -> authorizeHttp
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2Resource ->
+                        oauth2Resource.opaqueToken(opaqueToken -> opaqueToken
+                                .introspectionUri("http://localhost:8082/oauth2/introspect")
+                                .introspectionClientCredentials("algafood-web", "1234"))
+                )
                 .build();
     }
 }
