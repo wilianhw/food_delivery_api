0181d14c49ccb3e0008b3d2883b01ae9dd9a678b
wilian.souza <wilian.souza@dtidigital.com.br>
2024-01-11 12:22:20 -0300

    Adjusted hyperlinks of pedidos.

--- src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.assembler;
 import com.algaworks.algafood.api.AlgaLinks;
 import com.algaworks.algafood.api.controller.PedidoController;
 import com.algaworks.algafood.api.model.PedidoModel;
+import com.algaworks.algafood.core.security.AlgaSecurity;
 import com.algaworks.algafood.domain.model.Pedido;
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
@@ -13,30 +14,34 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
 
     private final ModelMapper modelMapper;
     private final AlgaLinks algaLinks;
+    private final AlgaSecurity algaSecurity;
 
-    public PedidoModelAssembler(ModelMapper modelMapper, AlgaLinks algaLinks) {
+    public PedidoModelAssembler(ModelMapper modelMapper, AlgaLinks algaLinks, AlgaSecurity algaSecurity) {
         super(PedidoController.class, PedidoModel.class);
         this.modelMapper = modelMapper;
         this.algaLinks = algaLinks;
+        this.algaSecurity = algaSecurity;
     }
 
     @Override
     public PedidoModel toModel(Pedido pedido) {
-        PedidoModel pedidoModel = createModelWithId(pedido.getId(), pedido);
+        PedidoModel pedidoModel = createModelWithId(pedido.getCodigo(), pedido);
         modelMapper.map(pedido, pedidoModel);
 
         pedidoModel.add(algaLinks.linkToPedidos("pedidos"));
 
-        if (pedido.podeSerConfirmado()) {
-            pedidoModel.add(algaLinks.linkToConfirmacaoPedido(pedidoModel.getCodigo(), "confirmar"));
-        }
+        if (algaSecurity.podeGerenciarPedidos(pedido.getCodigo())) {
+            if (pedido.podeSerConfirmado()) {
+                pedidoModel.add(algaLinks.linkToConfirmacaoPedido(pedidoModel.getCodigo(), "confirmar"));
+            }
 
-        if (pedido.podeSerCancelado()) {
-            pedidoModel.add(algaLinks.linkToCancelamentoPedido(pedidoModel.getCodigo(), "cancelar"));
-        }
+            if (pedido.podeSerCancelado()) {
+                pedidoModel.add(algaLinks.linkToCancelamentoPedido(pedidoModel.getCodigo(), "cancelar"));
+            }
 
-        if (pedido.podeSerEntregue()) {
-            pedidoModel.add(algaLinks.linkToEntregaPedido(pedidoModel.getCodigo(), "entregar"));
+            if (pedido.podeSerEntregue()) {
+                pedidoModel.add(algaLinks.linkToEntregaPedido(pedidoModel.getCodigo(), "entregar"));
+            }
         }
 
         pedidoModel.add(algaLinks.linkToRestaurantes(pedido.getRestaurante().getId()));
