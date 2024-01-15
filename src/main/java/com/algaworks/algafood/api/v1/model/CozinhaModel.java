538b3ad7046ca992350e820259baba0620375203
wilian.souza <wilian.souza@dtidigital.com.br>
2024-01-15 14:05:49 -0300

    Implemented documentation to cozinhas.

--- src/main/java/com/algaworks/algafood/api/v1/model/CozinhaModel.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/CozinhaModel.java
@@ -1,14 +1,21 @@
 package com.algaworks.algafood.api.v1.model;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Relation(collectionRelation = "cozinhas")
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class CozinhaModel extends RepresentationModel<CozinhaModel> {
+
+    @Schema(example = "1")
     private Long id;
+
+    @Schema(example = "Mineira")
     private String nome;
 }
--- src/main/java/com/algaworks/algafood/api/v1/model/input/CozinhaInput.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/input/CozinhaInput.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.v1.model.input;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Setter
 public class CozinhaInput {
 
+    @Schema(example = "Mineira")
     @NotBlank
     private String nome;
 }
