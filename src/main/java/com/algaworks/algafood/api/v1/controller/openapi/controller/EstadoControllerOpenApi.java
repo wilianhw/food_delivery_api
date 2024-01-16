254f28f45d618dfc0134de25b28da4ce70666e9a
wilian.souza <wilian.souza@dtidigital.com.br>
2024-01-16 09:56:53 -0300

    Implemented documentation of Estado.

--- src/main/java/com/algaworks/algafood/api/v1/controller/openapi/controller/EstadoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/openapi/controller/EstadoControllerOpenApi.java
@@ -1,27 +1,70 @@
 package com.algaworks.algafood.api.v1.controller.openapi.controller;
 
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.algaworks.algafood.api.v1.model.EstadoModel;
 import com.algaworks.algafood.api.v1.model.input.EstadoInput;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Estado", description = "Gerencia estados")
 public interface EstadoControllerOpenApi {
-    EstadoModel buscar(@PathVariable Long estadoId);
 
+    @Operation(summary = "Busca estado por ID",
+            responses = {
+                    @ApiResponse(responseCode = "200"),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "ID do estado inválido",
+                            content = {@Content(schema = @Schema(ref = "Problema"))}),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Estado não encontrado",
+                            content = @Content(schema = @Schema(ref = "Problema")))
+            })
+    EstadoModel buscar(@Parameter(example = "1") Long estadoId);
+
+    @Operation(summary = "Lista todos os estados")
     CollectionModel<EstadoModel> listar();
 
-    EstadoModel cadastrar(@RequestBody @Valid EstadoInput estadoInput);
+    @Operation(summary = "Cadastra um novo estado")
+    EstadoModel cadastrar(EstadoInput estadoInput);
 
+    @Operation(summary = "Busca estado por ID",
+            responses = {
+                    @ApiResponse(responseCode = "200"),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "ID do estado inválido",
+                            content = {@Content(schema = @Schema(ref = "Problema"))}),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Estado não encontrado",
+                            content = @Content(schema = @Schema(ref = "Problema")))
+            })
     EstadoModel atualizar(
-            @PathVariable Long estadoId,
-            @RequestBody @Valid EstadoInput estadoInput
+            @Parameter(example = "1") Long estadoId,
+            EstadoInput estadoInput
     );
 
-    void deletar(@PathVariable Long restauranteId);
+    @Operation(summary = "Busca estado por ID",
+            responses = {
+                    @ApiResponse(responseCode = "204"),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "ID do estado inválido",
+                            content = {@Content(schema = @Schema(ref = "Problema"))}),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Estado não encontrado",
+                            content = @Content(schema = @Schema(ref = "Problema")))
+            })
+    void deletar(@Parameter(example = "1") Long restauranteId);
 }
--- src/main/java/com/algaworks/algafood/api/v1/model/input/EstadoInput.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/input/EstadoInput.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.v1.model.input;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,8 @@ import lombok.Setter;
 @Setter
 @Getter
 public class EstadoInput {
+
+    @Schema(example = "Nome do estado")
     @NotBlank
     private String nome;
 }
