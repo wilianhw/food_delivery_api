753a8d5df355ee3a3b4e8763bb64195d55d9e804
wilian.souza <wilian.souza@dtidigital.com.br>
2024-01-16 11:23:50 -0300

    Implemented documentation of Permissões.

--- src/main/java/com/algaworks/algafood/api/v1/controller/openapi/controller/PermissaoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/openapi/controller/PermissaoControllerOpenApi.java
@@ -4,9 +4,13 @@ import org.springframework.hateoas.CollectionModel;
 
 import com.algaworks.algafood.api.v1.model.PermissaoModel;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Permissões", description = "Gerencia as permissões")
 public interface PermissaoControllerOpenApi {
+    @Operation(summary = "Lista as permissões")
     CollectionModel<PermissaoModel> listar();
 }
